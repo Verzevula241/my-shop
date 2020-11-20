227d6f48427bfbaf3c97214e62ef6b1848737a0f
Mikhail Zapatrin <C:\Users\zmg\AppData\Roaming\The Bat!>
2020-11-20 13:34:11 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Route, NavLink} from 'react-router-dom'
+import ReactLogo from './img/crown.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    const divStyle = {
+        textAlign: 'center',
+        marginBottom: '40px'
+    }
+
+    return (
+      <div style={divStyle}>
+          <header>
+          <nav className="nav">
+              <img src={ReactLogo} alt=""></img>
+              <ul>
+                  <li>
+                      <NavLink to="/search" className="header-button"><span style={{paddingLeft: "1em"}}>Поиск</span></NavLink>
+                  </li>
+                  <li>
+                      <NavLink to="/video" className="header-button"><span style={{paddingLeft: "1em"}}>Закладки</span></NavLink>
+                  </li>
+              </ul>
+          </nav>
+          </header>
+          <div >
+              <Route path="/search"/>
+              <Route path="/video"/>
+          </div>
+      </div>
+    );
+  }
 }
-
 export default App;
